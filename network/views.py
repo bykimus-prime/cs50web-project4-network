2d01b34064c1d3621521b06c999c1a1eb6110faa
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-01-30 11:12:40 +0900

    Display posts on index page

--- network/views.py
+++ network/views.py
@@ -8,7 +8,10 @@ from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+   allPosts = Post.objects.all().order_by("id").reverse() # show posts by most recent
+   return render(request, "network/index.html", {
+      "allPosts": allPosts
+   })
 
 def new_post(request):
    if request.method == "POST":
@@ -19,52 +22,52 @@ def new_post(request):
       return HttpResponseRedirect(reverse(index))
 
 def login_view(request):
-    if request.method == "POST":
+   if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+      # Attempt to sign user in
+      username = request.POST["username"]
+      password = request.POST["password"]
+      user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "network/login.html", {
-                "message": "Invalid username and/or password."
-            })
-    else:
-        return render(request, "network/login.html")
+      # Check if authentication successful
+      if user is not None:
+         login(request, user)
+         return HttpResponseRedirect(reverse("index"))
+      else:
+         return render(request, "network/login.html", {
+            "message": "Invalid username and/or password."
+         })
+   else:
+      return render(request, "network/login.html")
 
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+   logout(request)
+   return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+   if request.method == "POST":
+      username = request.POST["username"]
+      email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "network/register.html", {
-                "message": "Passwords must match."
-            })
+      # Ensure password matches confirmation
+      password = request.POST["password"]
+      confirmation = request.POST["confirmation"]
+      if password != confirmation:
+         return render(request, "network/register.html", {
+            "message": "Passwords must match."
+         })
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "network/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "network/register.html")
+      # Attempt to create new user
+      try:
+         user = User.objects.create_user(username, email, password)
+         user.save()
+      except IntegrityError:
+         return render(request, "network/register.html", {
+            "message": "Username already taken."
+         })
+      login(request, user)
+      return HttpResponseRedirect(reverse("index"))
+   else:
+      return render(request, "network/register.html")
