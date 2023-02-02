5bf8fcf7cbec47c67acea8b96f6b73154cec971d
Alexander Rodzyanko <alexander.rodzyanko@gmail.com>
2023-02-02 13:11:24 +0900

    Fix admin page string display for likes

--- network/models.py
+++ network/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
    date = models.DateTimeField(auto_now_add=True)
 
    def __str__(self):
-      return f"Post {self.id} was made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
+      return f"Post {self.id} made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
 
 class Follow(models.Model):
    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userIsFollowing")
